bb6d8c355d1f2584754ad56ce924c6a98a2c2af0
Debian <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-09-18 01:59:45 +0000

    array unknown

--- rv64-array-max/array_max.s
+++ rv64-array-max/array_max.s
@@ -8,4 +8,19 @@ array_max:
 	#current_address = array_address
 	#next_address += next_address_value
 	#largest_address = array_address + array_count
-	ret
+    lw a0, 0(a0)
+    mv t0, a0
+    ret
+# int array_max(int *array, int count)
+array_max:
+   # array_max(array_address, array_count)
+   #     greatest_array_int = array_address[0]
+	#current_address = array_address
+	#next_address += next_address_value
+	#largest_address = array_address + array_count
+    lw t0, 0(a0)
+    lw t2, 0(a1)
+    blt t2, 2, 1f		
+    li a0, 666
+1:  mv a0, t0
+    ret
