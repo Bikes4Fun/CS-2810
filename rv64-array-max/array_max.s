566e93b74b4e2abf69e809aff3e3059183356ca7
Debian <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-09-21 19:02:10 +0000

    array running with random int return

--- rv64-array-max/array_max.s
+++ rv64-array-max/array_max.s
@@ -4,10 +4,16 @@
 # int array_max(int *array, int count)
 array_max:
    # array_max(array_address, array_count)
-   #     greatest_array_int = array_address[0]
-	#current_address = array_address
-	#next_address += next_address_value
-	#largest_address = array_address + array_count
-    ld a1, 0(a0)
-    ld a0, 0(a1)
-    ret
+   # a0 - array param
+   # a1 - array count param
+   # a2 - largest value
+   # a3 - i? current address?
+   ld a2, 0(a0)
+   li a3, 1
+   2: bge a3, a1, 1f
+   slli t0, a3, 3
+   ld a2, (t0)
+   addi a3, a3, 1
+   j 2b
+   1: li a0, 33
+   ret
