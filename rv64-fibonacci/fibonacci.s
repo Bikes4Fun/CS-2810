5724bc22a45a1f3e272b5a48d9f291f53e82c5ff
Deanna <garrett.deanna@gmail.com>
2023-09-25 15:26:24 -0600

    practicing commit messages

--- rv64-fibonacci/fibonacci.s
+++ rv64-fibonacci/fibonacci.s
@@ -10,7 +10,7 @@ fibonacci:
 	# num1 + num2 = t0
 	li a1, 1		# 
 	li a2, 1 
-	li a3, 2 (i)
+	li a3, 2
 	li t0, 0
 1:  add t0, a1, a2	# num1 + num2 = num3
 	ld a1, a2 		# override num1 with num2
@@ -18,4 +18,4 @@ fibonacci:
 	addi a3, a3, 1	# i += 1
 	blt a3, a0, 1b	# while i < n, return to num3 = num1+num2 
 	mv a0, a2 
-return a0
+ret
