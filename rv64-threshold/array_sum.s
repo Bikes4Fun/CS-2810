07bf468a2cb4c22482955b5cec581860a8f2866d
Deanna <garrett.deanna@gmail.com>
2023-10-01 19:19:04 -0600

    threshold testing - completed planning in notebook and transferred.

--- rv64-threshold/array_sum.s
+++ rv64-threshold/array_sum.s
@@ -3,33 +3,38 @@
 
 # int array_sum(int *array, int count, int threshold)
 array_sum:
-        # a0 array address
-        # a1 len of array
-        # a2 threshold - sum elements greater or equal 
-        # a3 sum - current total sum - initiate at 0
-        # t0 i: counter to += each iteration while < a1 len of array
-        # t1 increment register address by counter * increment
-        # t3 current value
+                # a0 = was array[a0] but now = sum
+                # a1 = array size
+                # a2 = threshold to compare if current >= threshold, sum += current
+                # a3 = start of array: reassigned from a0 to use a0 as sum
+                # a4 = current value
+                # t0 = i counter
+                # t1 = temp, noting as reserved for readability 
 
-        li a3, 0        #load SUM as 0
-        ld t3, 0(a0)    #load CURRENT as 0
-        li t0, 1        #load COUNTER as 0 !!!!!! testing w 1
-        li t1, 8        #load register incrementer as 8
-        ##if a0 > a2: add a2 to t2: else make a0 = 33: return
-        
-        bge t3, a2, 2f  #if current(t3) >= threshold(a2) branch and add, then return
-        
-        ld t3, 8(a0)    #manually load a new item into current
-        bge t3, a2, 2f  #if new current(t3) >= threshold(a2) branch and add
-        
-        ld t3, 16(a0)   #load our current value 16 beyond a0
-	bge t3, a2, 2f
+                # 1  = initiation of while loop
+                # 2  = exit
 
-3:      li a0, 33       #if none of them are >=, make a0 33
-        j 1             #jump to return
-2:	add a3, a3, t3  #to total(a3) load current(t3)
-	mv a0, a3       #move sum to return
-1:      ret
+                # |--- initiate variables ---|
+                addi    a3, a0, 0       # move address of a0 to new register
+                ld      a4, 0(a0)       # initiate current to value of a0
+                li      a0, 0           # initiate sum/return register to 0
+                li      t0, 0           # initiate i counter to 0
+                li      t1, 0                # do nothing with tX, but visualize its use as a temp
+
+                # |--- initiate while loop ---|
+        1:      bge     t0, a1, 2f         # if i >= size of array, branch to exit
+
+                # |---validated while loop---|
+                slli    t1, t0, 3         # calculate incrementor of new address: i * 2^3 ?
+                add     t1, a3, t1         # temp = address of new current = address of start plus incrementor 
+                ld      a4, 0(a3)           # current = address start[0]
+                addi    t0, t0, 1         # i+=1
+
+                # |--- if: current >= threshold else ---|
+                blt     a4, a1, 1          # if: current < threshold, start over, else: continue
+                add     a0, a4, a4         # else: sum += current
+                j       1
+        2:      ret
 
 
 # count some of the values in an array.
