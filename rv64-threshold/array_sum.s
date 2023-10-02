7fd93b40f6c72e64243f6d5283b3dee841599633
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-10-02 17:47:20 +0000

    threshold passed.

--- rv64-threshold/array_sum.s
+++ rv64-threshold/array_sum.s
@@ -3,10 +3,11 @@
 
 # int array_sum(int *array, int count, int threshold)
 array_sum:
-                # a0 = was array[a0] but now = sum
+                # a0 = array initial address
                 # a1 = array size
                 # a2 = threshold to compare if current >= threshold, sum += current
-                # a3 = start of array: reassigned from a0 to use a0 as sum
+
+                # a3 = sum
                 # a4 = current value
                 # t0 = i counter
                 # t1 = temp, noting as reserved for readability 
@@ -14,28 +15,29 @@ array_sum:
                 # 1  = initiation of while loop
                 # 2  = exit
 
-                # |--- initiate variables ---|
-             
-                ld      a4, 0(a0)       # initiate current to value of a0
-                li      a3, 0           # initiate sum/return register to 0
-                li      t0, 0           # initiate i counter to 0
-                li      t1, 0           # do nothing with tX, but visualize its use as a temp
 
-                # |--- initiate while loop ---|
-        1:      bge     t0, a1, 2f         # if i >= size of array, branch to exit
+                # |--- initiate variables ---|     
+                ld      a4, 0(a0)         # current (first)  value = array[0] 
+                li      a3, 0             # sum = 0
+                li      t0, 0             # i (counter) = 0
+                li      t1, 0             # visualize will be temp
 
-                # |---validated while loop---|
-                slli    t1, t0, 3         # calculate incrementor of new address: i * 2^3 ?
-                add     t1, a0, t1         # temp = address of new current = address of start plus incrementor 
-                ld      a4, 0(t1)           # current = address start[0]
-                addi    t0, t0, 1         # i+=1
+       		# |---initiate while loop---|
+        1:      bge     t0, a1, 2f        # if i >= len(array): exit
 
-                # |--- if: current >= threshold else ---|
-                blt     a4, a2, 1b          # if: current < threshold, start over, else: continue
-                add     a2, a2, a4         # else: sum += current
-                j       1b
-        2:      
-		mv	a0, a2
+       		# |---continue while loop---|
+                slli    t1, t0, 3         # increment = (i*2^3)  0*2^3=0, 1*2^3=8, 2*2^3=16, 3*2^3=24 ...etc
+                add     t1, a0, t1        # a0+t1 (address start+incrementor) = new address saved to t1 
+                ld      a4, 0(t1)         # current = value at t1[0] or address of a0[i*2^3]
+                addi    t0, t0, 1         # i++
+
+        	# |---if inside while loop ---|
+                blt     a4, a2, 1b         # if: current(a4) < threshold(a2) return to while; else:
+                add     a3, a3, a4         # sum += current
+                j       1b                 # return to while
+        
+        2:      		      
+		mv	a0, a3
                 ret
 
 
@@ -63,7 +65,3 @@ array_sum:
 #     }
 #     return sum
 # }
-
-# leaf function:
-#     no need to set up a stack frame.
-#     do everything using scratch registers.
