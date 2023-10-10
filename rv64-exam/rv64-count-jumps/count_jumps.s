e703d42355cfb38b36b1e07c3280115ff8093252
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-10-10 23:31:46 +0000

    update count jum midterm

--- rv64-exam/rv64-count-jumps/count_jumps.s
+++ rv64-exam/rv64-count-jumps/count_jumps.s
@@ -6,13 +6,16 @@ count_jumps:
     	#   array   = a0
     	#   size    = a1
    	#   count   = a2 = 0
-   	#   start   = t0 = size-1
-    	#   index   = a3 = start               
+   	#   t0 = 1
+    	#   index   = a3 = size - t0              
+	#   max     = a4 = index starting val
+			
 		li a2, 0              
 	   	li t0, 1                             
-	   	sub a3, a1, t0        
+	   	sub a3, a1, t0 
+		addi a4, a3, 0      
 	2:   	bltz a3, 1f       #if a3(index) < 0: break
-	   	blt a1, a3, 1f    #if a1(size) < a3(index) break 
+	   	blt a4, a3, 1f    #if a4(max) < a3(index) break 
 	   	sll t0, a0, a3      #t0 = address of a0 shifted index amount?
            	ld t0, 0(t0)        #t0 = value at t0 
 	   	add a3, a3, t0      #a3 = index + t0 (current value at array[index])
