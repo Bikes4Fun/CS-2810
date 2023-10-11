c44368e03af5404e37f23ff139ea09f68cf326c1
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-10-11 06:19:34 +0000

    can't grind sync

--- rv64-exam/rv64-stoi/stoi.s
+++ rv64-exam/rv64-stoi/stoi.s
@@ -3,29 +3,21 @@
 stoi:
                 # your code goes here
 
-    		li a1, 0     # 
 
     1: 		ld a2, 0(a0)  
     		li a3, 48      # ASCII '0'
     		li t0, 57      # ASCII '9'
    	 
-    		beq a2, 45, 3f
+    		#beq a2,...
     		blt a2, a3, 4f
 	
     2:		bge a2, t0, 4f
-    		mul a0, a0, 10  # Multiply n by 10
-    		add a0, a0, a2  # Add the digit to n
-    		addi a0, a0, 1  # Move to the next character
-    		j 1b
+		li t1, 10
+    		mul a1, a0, t1  # Multiply n by 10
+    		add a1, a1, a2  # Add the digit to n
+    		ld a2, 1(a0)  # Move to the next character
+    		#j 1b
     
-   3:
-    		addi a0, a0, 1
-    li a1, 1  # Set a1 to 1 to indicate a negative number
-    j 1b
-    
-   4: 
-    beqz a1, 3f  # If it's not a negative number, skip the negation
-    neg a0, a0  # Negate the result
-    
-    ret
+    4:		#mv a0 ...
+	ret
 
