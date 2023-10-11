ae0031ca2655c8b5565bffc1fa9f4cfe4ad0f808
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-10-11 04:34:12 +0000

    :(

--- rv64-exam/rv64-count-jumps/count_jumps.s
+++ rv64-exam/rv64-count-jumps/count_jumps.s
@@ -18,9 +18,12 @@ count_jumps:
 	   	blt a4, a3, 1f    #if a4(max) < a3(index) break
 		slli t0, a3, 0 	#t0 = address of a0 shifted index amount? 
 	   	add t0, a0, t0      #t0 = address of a0 shifted index amount?
-           	ld t0, 0(t0)        #t0 = value at t0 
-	   	add a3, a3, t0      #a3 = index + t0 (current value at array[index])
-	   	addi a2, a2, 1
+           	ld t0, 0(t0)        #t0 = value at t0
+		sltiu t0, t0,  0
+		j 1f
+		addi t1, a3, 0
+		add a3, t1, t0      #a3 = index + t0 (current value at array[index])
+		addi a2, a2, 1
 		j 2b
-	1:	mv a0, a2                
+	1:	mv a0, t0                
 	ret
