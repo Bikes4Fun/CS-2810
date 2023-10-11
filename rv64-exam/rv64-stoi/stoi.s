07d440b42ca74972cb3c414526e9b88f79a11f4f
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-10-11 06:09:45 +0000

    m

--- rv64-exam/rv64-stoi/stoi.s
+++ rv64-exam/rv64-stoi/stoi.s
@@ -2,4 +2,30 @@
                 .text
 stoi:
                 # your code goes here
-                ret
+
+    		li a1, 0     # 
+
+    1: 		ld a2, 0(a0)  
+    		li a3, 48      # ASCII '0'
+    		li t0, 57      # ASCII '9'
+   	 
+    		beq a2, 45, 3f
+    		blt a2, a3, 4f
+	
+    2:		bge a2, t0, 4f
+    		mul a0, a0, 10  # Multiply n by 10
+    		add a0, a0, a2  # Add the digit to n
+    		addi a0, a0, 1  # Move to the next character
+    		j 1b
+    
+   3:
+    		addi a0, a0, 1
+    li a1, 1  # Set a1 to 1 to indicate a negative number
+    j 1b
+    
+   4: 
+    beqz a1, 3f  # If it's not a negative number, skip the negation
+    neg a0, a0  # Negate the result
+    
+    ret
+
