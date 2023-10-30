22a7832d579de6b86d525fe2dc2a9f2406b6e492
Deanna <garrett.deanna@gmail.com>
2023-10-30 12:52:05 -0600

    laptop oct 30 start

--- sudoku/pencil_marks.s
+++ sudoku/pencil_marks.s
@@ -3,19 +3,13 @@
 
 # count_bits(n) -> # of bits set in n (only counting bits 0-9 inclusive)
 count_bits:
-        #     while index <= 9:
-        #         mask = 1<<index
-        #         temp = n & mask
-        ##         if temp != 0:
-        #             count += 1
-        #         index += 1
-        #     return count
-
+    # leaf
         li t0, 1	# mask
         li t1, 0        # result of AND t0 with a0
         li a1, 0        # count = 0
         li a2, 0        # index = 0
         li a3, 9        # range max = 9
+    # beqz a0, 2f
         beqz a0, 2f
 
     1:  bgt  a2, a3, 2f  # if index > range max: branch to exit sequence, else continue 'while'
@@ -29,11 +23,12 @@ count_bits:
     3:  addi a1, a1, 1   # else: count += 1
         j    4b
 
-    2:  mv  a0, a1
-        ret
+    2:  mv  a0, a1       # return how many bits in set
+    ret
 
 # get_used(board, group) -> used
 get_used:
+    # stack:
             #                       64
             #       +-------------+
             #       | saved ra    | 56
@@ -53,7 +48,7 @@ get_used:
             # sp -> |             | 0
             #       +-------------+
 
-            #prelude
+    #prelude
             addi    sp, sp, -48
             sd      ra, 40(sp)
             sd      s4, 32(sp)
@@ -64,7 +59,6 @@ get_used:
 
             mv      s0, a0
             mv      s1, a1
-            #       s2 = value for count bits to count
             li      s3, 0            # used
             li      s4, 0            # iter
 
@@ -83,8 +77,7 @@ get_used:
     3:      addi    s4, s4, 1        # Return the “used” list as a set with one bit per used number (currently returning total used)
             j       1b
     2:      mv      a0, s3
-
-            # postlude
+    # postlude
             ld      ra, 40(sp)
             ld      s4, 32(sp)
             ld      s3, 24(sp)
@@ -92,12 +85,12 @@ get_used:
             ld      s1, 8(sp)
             ld      s0, 0(sp)
             addi    sp, sp, 48
-            ret
+    ret
 
 # clear_used(board, group, used)
 clear_used:
-
-# 64
+    # stack:
+            #                       64
             #       +-------------+
             #       | saved ra    | 56
             #       +-------------+
@@ -115,7 +108,7 @@ clear_used:
             #       +-------------+
             # sp -> |             | 0
 		
-            #prelude
+    # prelude:
             addi    sp, sp, -72
             sd      ra, 64(sp)
             sd      s7, 56(sp)
@@ -163,8 +156,7 @@ clear_used:
             # |+--- end loop ---+|
 
     2:      mv      a0, s4
-
-            # postlude
+    # postlude
             ld      ra, 64(sp)
             ld      s7, 56(sp)
             ld      s6, 48(sp)
@@ -175,8 +167,8 @@ clear_used:
             ld      s1, 8(sp)
             ld      s0, 0(sp)
             addi    sp, sp, 72
-            ret
+    ret
 
 # pencil_marks(board, table)
 pencil_marks:
-                ret
+        
