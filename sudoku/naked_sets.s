b6250d743a702c4e5d8f219a599753506c980b8a
Bikes4Fun <admin@ip-172-26-15-252.us-west-2.compute.internal>
2023-11-16 02:50:18 +0000

    .

--- sudoku/naked_sets.s
+++ sudoku/naked_sets.s
@@ -110,7 +110,7 @@ single_pass:
             # *   If the size of the subset matches the size of the candidate set, you have found a naked set of that size, so:
             # *   call `clear_others` to cross those values off any other cells
     # Track and return if changes occured (clear_others)
-    
+
     # prelude:
         addi    sp, sp, -72
         sd      ra, 64(sp)
@@ -131,14 +131,14 @@ single_pass:
         mv      s1, a1          # saved group
         li      s2, 1           # iteration(key)?
         #mv     s3, a0          # bits from key: return of first count_bits(key) call
-        #don't need. nvm this is subset? mv s4, a0  # gathered set, return of gather_set(board, group, key) call
+        #mv     s4, a0          # gathered set, return of gather_set(board, group, key) call
         li      s5, 0           # changes
         #mv     s6, a0          # second call to count_bits? this is candidate set?
         li      s7, 510         # max iteration
 
-    1:  #main   iteration       0-510 inclusive
-        bgt     s2, s7, 3f  
-        
+    1:  #main   iteration       1-510 inclusive
+        bgt     s2, s7, 3f
+
     #   call    count_bits:     on the key to see how big the subset is #of bits set in n (only counting bits 0-9 inclusive)
         mv      a0, s2          # iteration value 1-510 = key. move to a0 as argument for count_bits
         call    count_bits
@@ -147,21 +147,21 @@ single_pass:
     #   call    gather_set:     gather candidates in the pencil marks for those cells set of pencil marks for cells identified by key
         mv      a0, s0          # board into a0 to call gather_set
         mv      a1, s1          # group into a1 to call gather_set
-        mv      a2, s3          # key(iteration?) passed to gather_set? or counted key?
+        mv      a2, s2          # key(iteration?) passed to gather_set? or counted key?
         call    gather_set
         mv      s4, a0          #shouldn't need to save this?(possibly the error)
-        
+
     #   call    count_bits:     on gathered to see the combined number of candidate values used by that subset
         call    count_bits      # use return from gather_set which is already in a0?
         mv      s6, a0          # candidate = count_bits second time with gather_set return, obtaining 'candidate'
-       
+
     #   If      sets match:     continue, else: break
         bne     s3, s6, 2f      #calculate next iteration?
-        
+
     #   call    clear_others:   cross values off cells: 0(no change) 1(changed)
         mv      a0, s0          # should be board
         mv      a1, s1          # should be group
-        mv      a2, s3          # s2 should be key/iter? (changed to counted bits of iter)
+        mv      a2, s2          # s2 should be key/iter? (changed to counted bits of iter)
         mv      a3, s4          # s3 should be subset/current set? nvm s3 was bits in the key, s4 is subset?
         call    clear_others
         beqz    a0, 2f          # if 0(no change) perform iteration
@@ -170,7 +170,7 @@ single_pass:
     2:  #iter   calculations
         addi    s2, s2, 1
         j       1b
-    
+
     3:  mv      a0, s5          # if changes from clear others
 
     # postlude
