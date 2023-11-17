e16eec8ed50c7f03ea0724386a2be603379a352a
Deanna <garrett.deanna@gmail.com>
2023-11-16 23:56:18 -0700

    num 10 psudo

--- sudoku/guess.s
+++ sudoku/guess.s
@@ -23,44 +23,49 @@ guess:
         sd      s1, 8(sp)
         sd      s0, 0(sp)
 
-        board = s0 (mv s0, a0)
-        table = s1 (mv s1, a1)
-        position = s2 (mv s2, s2)
-        position_value:
-            slli    s3, s2, 1
-            add     s3, s3, s0
-            lh      s3, (s3)
-        guess = 1
-        max_guess = 9
-        mask = slli     mask, guess, 1
+        mv      s0, a0          # board = s0              
+        mv      s1, a1
+        mv      s2, s2
+        #position_value:
+        slli    s3, s2, 1
+        add     s3, s3, s0
+        lh      s3, (s3)
+        li      s4, 1           # guess
+        
+        li      t1, 9           # max_guess
+        #mask = slli     mask, guess, 1
 
+        #get current cell value
+        #cell_value = board[position_value]
 
-        // get current cell value
-        cell_value = board[position_value]
+    1:  #while loop over guesses (1 to 9)
+        li      t1, 9
+        bgt     s4, t1, end      # while guess <= max_guess: 
+        # mask = 1 << guess
+        ##if (cell_value & mask) 
+        
+        mv      a0, s0
+        mv      a1, s1
+        call    guess           #(guess, position)
 
-        // Iterate over guesses (1 to 9)
-    
-        while guess <= max_guess: 
-            mask = 1 << guess
-            if (cell_value & mask) 
-            Print guess (guess, position)
+        #Set cell to the guess value
+        #board[position] = mask
 
-            // Set cell to the guess value
-            board[position] = mask
+        mv      a0, s0
+        mv      a1, s1
+        call    solve
+        #result = solve(board, table)
 
-            mv      a0, s0
-            mv      a1, s1
-            call    solve
-            result = solve(board, table)
+        bnez    a0, continue
+        li      a0, 0
+        ret
 
-            bnez    a0, continue
-            li      a0, 0
-            ret
-            
-            guess++
+    continue:
+        addi    s4, s4, 1    
 
-        // No solution
-        return 1; // Failure
+    no_solution:
+        li      a0, 1
+        ret
 
 
     # postlude
@@ -76,4 +81,4 @@ guess:
         addi    sp, sp, 72
 
 
-                ret
+    ret
