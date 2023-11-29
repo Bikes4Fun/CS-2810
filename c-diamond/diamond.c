0bd2be977a3c70d9294091e6aedac24b1d8b79f5
Deanna <garrett.deanna@gmail.com>
2023-11-29 16:01:13 -0700

    do while

--- c-diamond/diamond.c
+++ c-diamond/diamond.c
@@ -8,17 +8,29 @@ void print_diamond(int n){
     }
     else {
         //char *star = malloc(n+1);
-        char *spaces = malloc(n+1);
+        char *spaces = malloc(n+n+1);
         for (int i = 0; i < n; i++){
             spaces[i] = ' ';
             //printf("%s\n", spaces);
         }
-        spaces[n]='\0';
-        for (int i = 0; i < n; i++){
-            spaces[n-i-1] = '*';
-            spaces[n+i] = '*';
+        spaces[n-1]='*';
+        printf("%s\n", spaces);
+        spaces[n]= '*';
+        int i = 1;
+        do {
+            spaces[n-1-i] = '*';
+            spaces[n+i+1] = '*';
+            i++;
             printf("%s\n", spaces);
+        } while(spaces[0] != '*');
+        for (i = n; i > 0; i--) {
+            spaces[n-i] = ' ';
+            spaces[n+i] = '\n'; 
+            
+            printf("%s", spaces);
         }
+
+
         //printf("\n");
         free(spaces);
     }
