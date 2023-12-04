702c66f9c4f21d677ec612906e7be244b84ea4fc
Deanna <garrett.deanna@gmail.com>
2023-12-03 20:07:36 -0700

    loop structure made. need to read each line and verify. outter loop is while file line not null. realloc has error handling. running but not working.

--- c-wordle/wordlist.c
+++ c-wordle/wordlist.c
@@ -31,26 +31,29 @@ char **read_word_list(char *filename) {
     int num_word_elements = 0;                      // track number of word in array
     int word_list_capacity = 32*word_bytes;         // track amount of memory that was allocated for word_list array
     char line[16];
-    FILE *word_file = fopen(filename, "r");                // open file?
-    bool line_null = (fgets(line, 16, word_file) == NULL);
-    //read file
-    //for each line in file
-    //check if num_word_elements < word_list_capacity
-    do {
+    FILE *word_file = fopen(filename, "r");         // open file?
+    
+    //read file     //for each line in file     //check if num_word_elements < word_list_capacity
+    while (fgets(line, 16, word_file) != NULL) {
         if ((num_word_elements*word_bytes) < word_list_capacity) {
             if ((sizeof(line) < 5) && (line[5] == '\n')) { //psuedocode: if size of line is 5 char and a \n
-                char *word;
-                strcpy(word, line);// add to array 
+                //strpy(word, line);// add to array 
                 num_word_elements ++;
             }
         }
+        
         else {
+            char **new_list = realloc(word_list, (word_list_capacity*2));
+            if (!new_list) {                            // Handle memory allocation error
+                free_word_list(word_list);              // Remember to free previously allocated memory
+                fclose(word_file);
+                return NULL;
+            }
+            word_list = new_list;
             word_list_capacity *= 2;
-            word_list = realloc(word_list, 32*word_bytes);
         }
         fgets(line, 16, word_file);
-    } while (line_null);
-
+    }
     fclose(word_file);
     return word_list;
 }
