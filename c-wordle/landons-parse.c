c978de35f072b844fe51b4ec8ed65326afb40175
Deanna <garrett.deanna@gmail.com>
2023-12-05 20:11:29 -0700

    Update landons-parse.c
    Landon

--- c-wordle/landons-parse.c
+++ c-wordle/landons-parse.c
@@ -1,5 +1,29 @@
 #include "wordle.h"
 
 guess parse_guess(char *line) {
+        guess guess;
+        char *arrow = line;
 
-}
+        for(int i = 0; i < 5; i++){
+                 char meme = *arrow;
+        if(meme == '['){
+                //Exact hit
+                guess.letters[i] = *(arrow =1
+                guess.feedback[i] = EXACT_HIT;
+                arrow += 3;
+         }
+        else if(meme == '('){
+                //Partial hit
+               guess.letters[i] = *(arrow +1);
+               guess.feedback[i] = PARTIAL_HIT;
+               arrow += 3;
+        }
+       else{
+              //Miss
+              guess.letters[i] = meme;
+              guess.feedback[i] = MISS;
+              arrow += 1;
+       }
+        }
+      guess.letters[5] = '\0';
+      return guess;)
