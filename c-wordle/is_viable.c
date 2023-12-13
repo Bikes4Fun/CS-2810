dea3a0f24bf333c43bbcb772ad73bb13f5576b65
Deanna <garrett.deanna@gmail.com>
2023-12-12 18:40:00 -0700

    wordle passes. about to run grind.
    q

--- c-wordle/is_viable.c
+++ c-wordle/is_viable.c
@@ -4,50 +4,59 @@
 #include <string.h>
 #include "wordle.h"
 
+// Check if a candidate word is a viable option
 bool is_viable_candidate (char *candidate, guess *guesses, int guess_count){
-    for (int i = 0; i < guess_count; i++){
-        char copy[6];
-        strcpy(copy, candidate);
+    for (int guess_index = 0; guess_index < guess_count; guess_index++){
+        // Copy the candidate word
+        char candidate_copy[6];
+        strcpy(candidate_copy, candidate);
 
-        for (int j = 0; j < 5; j++){
-            if (guesses[i].feedback[j] == EXACT_HIT){
-                if (copy[j] != guesses[i].letters[j]){
+        // Check each letter
+        for (int letter_index = 0; letter_index < 5; letter_index++){
+            if (guesses[guess_index].feedback[letter_index] == EXACT_HIT){
+                // Check letter for exact hit
+                if (candidate_copy[letter_index] != guesses[guess_index].letters[letter_index]){
+                    // if it's not the same, 
                     return false;
                 }
-                copy[j] = '_';
+                // else, mark the letter as "_"
+                candidate_copy[letter_index] = '_';
             }
             for (int j = 0; j < 5; j++){
-                if (guesses[i].feedback[j] == PARTIAL_HIT){
-                    if (copy[j] == guesses[i].letters[j]){
+                // check if the letter is also a partial hit
+                if (guesses[guess_index].feedback[j] == PARTIAL_HIT){
+                    if (candidate_copy[j] == guesses[guess_index].letters[j]){
                         return false;
                     }
                 }
             }
         }
         for (int j = 0; j < 5; j++){
-            if (guesses[i].feedback[j] == PARTIAL_HIT){
-                int a = 0;
+            if (guesses[guess_index].feedback[j] == PARTIAL_HIT){
+                int found = 0;
                 for (int k = 0; k < 5; k++){
-                    if (copy[k] == guesses[i].letters[j]){
-                        copy[k] = '_';
-                        a = 1;
+                    if (candidate_copy[k] == guesses[guess_index].letters[j]){
+                        candidate_copy[k] = '_';
+                        found = 1;
                         break;
                     }
                 }
-                if(!a){
+                // if no match, it's not viable.
+                if(!found){
                     return false;
                 }
             }
         }
         for (int j = 0; j < 5; j++){
-            if (guesses[i].feedback[j] == MISS){
+            if (guesses[guess_index].feedback[j] == MISS){
                 for (int k = 0; k < 5; k++){
-                    if (copy[k] == guesses[i].letters[j]){
+                    if (candidate_copy[k] == guesses[guess_index].letters[j]){
                         return false;
                     }
                 }
             }
         }
     }
+    // if all checks were true, it's a viable option.
     return true;
 }
--- c-wordle/recommend.c
+++ c-wordle/recommend.c
@@ -1,3 +1,7 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "wordle.h"
 
 // a convenient struct to store a potential guess along with its score
@@ -7,4 +11,44 @@ struct recommendation {
 };
 
 void recommend(char **word_list, guess *guesses, int guess_count) {
+    struct recommendation recommendations[RECOMMENDATION_COUNT + 1];
+    int num_recommendations = 0;
+
+    for (int i = 0; word_list[i] != NULL; i++) {
+        char *word = word_list[i];
+
+        if (!is_viable_candidate(word, guesses, guess_count)) {
+            continue;
+        }
+
+        int word_score = score(word_list, word, guesses, guess_count);
+
+        // Add to recommendations
+        recommendations[num_recommendations].score = word_score;
+        strcpy(recommendations[num_recommendations].word, word);
+        num_recommendations++;
+
+        // Bubble up the new recommendation to its correct position
+        for (int j = num_recommendations - 1; j > 0; j--) {
+            if (recommendations[j].score > recommendations[j - 1].score ||
+                (recommendations[j].score == recommendations[j - 1].score && strcmp(recommendations[j].word, recommendations[j - 1].word) < 0)) {
+                struct recommendation temp = recommendations[j];
+                recommendations[j] = recommendations[j - 1];
+                recommendations[j - 1] = temp;
+            } else {
+                break;
+            }
+        }
+
+        // Limit the list to RECOMMENDATION_COUNT
+        if (num_recommendations > RECOMMENDATION_COUNT) {
+            num_recommendations = RECOMMENDATION_COUNT;
+        }
+    }
+
+    // Print top recommendations
+    for (int i = 0; i < num_recommendations; i++) {
+        printf("%d: %s\n", recommendations[i].score, recommendations[i].word);
+    }
+
 }
